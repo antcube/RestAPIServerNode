c9905f0b07de8d6d2821207ceb6d50046721f910
antcube <aqr885@gmail.com>
2024-12-10 19:55:32 -0500

    Instalando TS, tsx, Nodemon, y prueba en index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const suma = (a: number, b: number): void => {
+    const adicion = a + b;
+    console.log("La suma de " + a + " + " + b + " es: " + adicion);
+}
+
+suma(5, 20);
+console.log('Hola desde index.ts');
