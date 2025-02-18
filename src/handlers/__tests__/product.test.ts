df669e4243252c5bc3d34c8864701b5ea1f9c048
antcube <aqr885@gmail.com>
2025-02-18 17:59:52 -0500

    Testing endpoint para metodo DELETE para un producto

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -193,3 +193,34 @@ describe('PUT /api/products/:id', () => {
         expect(response.body).not.toHaveProperty('errors')
     })
 })
+
+describe('DELETE /api/products/:id', () => {
+    it('Should check a valid ID in the URL', async () => {
+        const response = await request(server)
+            .delete('/api/products/not-valid-id')
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors[0].msg).toBe('The id must be an integer')
+    })
+    it('Should return a 404 response if the product does not exist', async () => {
+        const productId = 2000
+        const response = await request(server)
+            .delete(`/api/products/${productId}`)
+
+        expect(response.status).toBe(404)
+        expect(response.body.message).toBe('Product not found')
+
+        expect(response.status).not.toBe(200)
+    })
+    it('Should delete an existing product', async () => {
+        const response = await request(server)
+            .delete('/api/products/1')
+        
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe('Product deleted successfully')
+
+        expect(response.status).not.toBe(404)
+        expect(response.status).not.toBe(400)
+    })
+})
