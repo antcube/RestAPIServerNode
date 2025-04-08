80d35bc7b339c7283d0e794ea91c4d3337987a60
antcube <aqr885@gmail.com>
2025-04-08 18:25:22 -0500

    Generando variable de entorno y CORS para aceptar los dominios permitidos

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Express} from "express";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
 import swaggerUi from "swagger-ui-express";
 import router from "./router";
 import db from "./config/db";
@@ -26,6 +27,28 @@ export const connectDB = async (): Promise<void> => {
     }
 }
 connectDB();
+
+// Middleware to enable CORS
+const corsOptions: CorsOptions = {
+    origin: function(origin, callback) {
+        const isDevelopment = process.env.NODE_ENV === "development";
+        
+        if(isDevelopment) {
+            if(!origin || origin === process.env.FRONTEND_URL || origin === "http://localhost:4000") {
+                callback(null, true);
+            } else {
+                callback(new Error("CORS not allowed"));
+            }
+        } else {
+            if(origin === process.env.FRONTEND_URL) {
+                callback(null, true);
+            } else {
+                callback(new Error("CORS not allowed"));
+            }
+        }
+    }
+}
+server.use(cors(corsOptions));
 
 // Define the routes
 server.use("/api/products", router);
