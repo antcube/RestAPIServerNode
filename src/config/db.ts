9a813ff82de55330596d9f65d4eb47bb645f1fb5
antcube <aqr885@gmail.com>
2025-04-11 19:30:26 -0500

    Creando el build, excluyendo algunos archivos no necesarios para produccion

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*.ts'],
+    models: [__dirname + '/../models/**/*.{ts,js}'], // Path to your model files
     logging: false // Disable logging when running tests
 });
 
